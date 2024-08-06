440baa7819347e62588e823aa03990073b3f0ed5
YCHM0304 <samsamsam876@gmail.com>
2024-08-06 16:10:49 +0800

    add api

---
+++ api.py
@@ -0,0 +1,288 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Dict, List, Any, Optional
+import akasha
+import requests
+import os
+from bs4 import BeautifulSoup
+
+app = FastAPI()
+final_result = {}
+report_config = {
+    "theme": "",
+    "titles": {},
+    "links": []
+}
+
+
+model = "openai:gpt-4o"
+QA = akasha.Doc_QA(model=model, max_doc_len=8000)
+summary = akasha.Summary(chunk_size=1000, max_doc_len=7000)
+
+class ReportRequest(BaseModel):
+    theme: str
+    titles: Dict[str, List[str]]
+    links: List[str]
+    openai_config: Optional[Dict[str, Any]]
+
+class ReprocessContentRequest(BaseModel):
+    command: str
+    openai_config: Optional[Dict[str, Any]]
+
+def load_openai(config: dict) -> bool:
+    """delete old environment variable and load new one.
+
+    Args:
+        config (dict): dictionary may contain openai_key, azure_key, azure_base.
+
+    Returns:
+        bool: load success or not
+    """
+    if "OPENAI_API_KEY" in os.environ:
+        del os.environ["OPENAI_API_KEY"]
+    if "AZURE_API_BASE" in os.environ:
+        del os.environ["AZURE_API_BASE"]
+    if "AZURE_API_KEY" in os.environ:
+        del os.environ["AZURE_API_KEY"]
+    if "AZURE_API_TYPE" in os.environ:
+        del os.environ["AZURE_API_TYPE"]
+    if "AZURE_API_VERSION" in os.environ:
+        del os.environ["AZURE_API_VERSION"]
+
+    if "openai_key" in config and config["openai_key"] != "":
+        os.environ["OPENAI_API_KEY"] = config["openai_key"]
+
+        return True
+
+    if ("azure_key" in config and "azure_base" in config
+            and config["azure_key"] != "" and config["azure_base"] != ""):
+        os.environ["AZURE_API_KEY"] = config["azure_key"]
+        os.environ["AZURE_API_BASE"] = config["azure_base"]
+        os.environ["AZURE_API_TYPE"] = "azure"
+        os.environ["AZURE_API_VERSION"] = "2023-05-15"
+        return True
+
+    return False
+
+@app.get("/check_result")
+async def check_result():
+    if not final_result:
+        return False
+    if not report_config["theme"]:
+        return False
+    if not report_config["titles"]:
+        return False
+    if not report_config["links"]:
+        return False
+    return True
+
+@app.post("/generate_report")
+async def generate_report(request: ReportRequest):
+    global final_result
+    report_config["theme"] = request.theme
+    report_config["titles"] = request.titles.copy()
+    report_config["links"] = request.links.copy()
+    if not load_openai(config=request.openai_config):
+        raise HTTPException(status_code=400, detail="請提供OpenAI或Azure的API金鑰")
+    result = {}
+    contexts = []
+
+    for title, subtitle in request.titles.items():
+        format_prompt = f"以{request.theme}為主題，請你總結撰寫出與\"{title}\"相關的內容，其中需包含{subtitle}，不需要結論，不需要回應要求。"
+        for link in request.links:
+            try:
+                response = requests.get(link)
+                response.raise_for_status()
+                soup = BeautifulSoup(response.content, 'html.parser')
+                texts = soup.get_text()
+            except requests.exceptions.RequestException as e:
+                raise HTTPException(status_code=400, detail=f"Error fetching content: {str(e)}")
+
+            contexts.append(
+                summary.summarize_articles(
+                    articles=texts,
+                    format_prompt=format_prompt,
+                )
+            )
+
+        response = QA.ask_self(
+            prompt=f"將此內容以客觀角度進行融合，避免使用\"報告中提到\"相關詞彙，避免修改專有名詞，避免做出總結，直接撰寫內容，避免回應要求。",
+            info=contexts,
+            model="openai:gpt-4"
+        )
+        result[title] = response
+        contexts = []
+
+    previous_result = ""
+    for value in result.values():
+        previous_result += value
+
+    result["內容摘要"] = summary.summarize_articles(
+        articles=previous_result,
+        format_prompt=f"將內容以{request.theme}為主題進行摘要，將用字換句話說，意思不變，不需要結論，不需要回應要求。",
+        summary_len=500
+    )
+    final_result = result.copy()
+    return result
+
+
+
+@app.post("/reprocess_content")
+async def reprocess_content(request: ReprocessContentRequest):
+    if await check_result() == False:
+        raise HTTPException(status_code=400, detail="請先使用generate_report生成報告")
+    if final_result != {}:
+        new_request= QA.ask_self(
+            prompt=f"""使用者輸入了以下修改要求:
+            ----------------
+            {request.command}
+            ----------------
+            從給定的list和使用者的修改要求中找出使用者要求修改的部分為何，以及要如何修改，前面不須加上"回覆:"。
+
+            範例:
+            <給定的list>
+            ["全球電池市場概況", "技術發展趨勢", "應用領域與市場機會", "政策與供應鏈分析"]
+            <要求>
+            技術發展趨勢的部分我覺得不夠完整，請加入更多新興電池技術的內容。
+            <回覆>
+            修改部分: 技術發展趨勢
+            修改內容: 加入更多新興電池技術的內容
+
+            若要求的部分不在給定的list當中，請回覆"報告中無此部分，請確認後再提出修改要求"，前面不須加上"回覆:"。
+
+            範例:
+            <給定的list>
+            ["全球電池市場概況", "技術發展趨勢", "應用領域與市場機會", "政策與供應鏈分析"]
+            <要求>
+            我覺得電池發展歷史的部分不夠完整，請幫我修改。
+            <回覆>
+            報告中無此部分，請確認後再提出修改要求。    <----- 只需回覆這句話，前面不須加上"回覆:"
+
+            若修改要求中雖然有錯別字或文法錯誤，但仍能理解要求，也請回覆"修改部分: <修改部分> 修改內容: <修改內容>"，前面不須加上"回覆:"。
+
+            範例:
+            <給定的list>
+            ["全球電池市場概況", "技術發展趨勢", "應用領域與市場機會", "政策與供應鏈分析"]
+            <要求>
+            我覺得全球電池試場概況的部分不夠完整，請加入更多市場規模與增長趨勢的內容。
+            <回覆>
+            修改部分: 全球電池市場概況
+            修改內容: 加入更多市場規模與增長趨勢的內容
+
+            若只有提出修改的要求，沒有指定要修改的部分，請回覆"不知道您想要修改哪一部分，請提供更多資訊"。避免自動回覆list中所有項目。前面不須加上"回覆:"。
+            只要要求中沒有看到list中的element，就回覆"不知道您想要修改哪一部分，請提供更多資訊"。
+            範例:
+            <給定的list>
+            ["全球電池市場概況", "技術發展趨勢", "應用領域與市場機會", "政策與供應鏈分析"]
+            <要求>
+            去除2025年以前的資料。  <----- 只有提出修改的要求，沒有指定要修改的部分
+            <回覆>
+            不知道您想要修改哪一部分，請提供更多資訊    <----- 需要修改的部分不明確，也沒有list中的element，因此只需回覆這句話，前面不須加上"回覆:"
+
+            若無明確指定想要如何修改，回覆"無法理解您的修改要求，請提供更多資訊"，前面不須加上"回覆:"。
+
+            <給定的list>
+            ["全球電池市場概況", "技術發展趨勢", "應用領域與市場機會", "政策與供應鏈分析"]
+            <要求>
+            我覺得技術發展趨勢的部分有問題。
+            <回覆>
+            無法理解您的修改要求，請提供更多資訊    <----- 只需回覆這句話，前面不須加上"回覆:"
+            """,
+            info=[key for key in final_result.keys()],
+            model="openai:gpt-4"
+        )
+
+        try:
+            part = new_request.split("修改部分: ")[1].split("\n修改內容: ")[0]
+            mod_command = new_request.split("修改部分: ")[1].split("\n修改內容: ")[1]
+            if part in final_result:
+                previous_context = final_result[part]
+
+                modification = QA.ask_self(
+                    prompt=f"""從以下修改要求和提供的內容判斷是否需要重新爬取資料。大部分情況需要加入新的資料進去時會，
+                    需要爬取資料，此時需要回覆"y"。否則回覆"n"。若修改要求和提供的內容完全沒有關聯性或者無法判斷時，請回覆"unknown"。
+                    ----------------
+                    {mod_command}
+                    ----------------
+                    y的範例:
+                    <修改要求>
+                    加入非洲市場區域分析。
+                    <提供的內容>
+                    台灣的電池產業發展迅速，主要市場區域包括亞洲、美洲和歐洲。
+                    <回覆>
+                    y
+
+                    n的範例:
+                    <修改要求>
+                    消去亞洲市場區域分析。
+                    <提供的內容>
+                    台灣的電池產業發展迅速，主要市場區域包括亞洲、美洲和歐洲。
+                    <回覆>
+                    n
+
+                    unknown的範例:
+                    <修改要求>
+                    加入非洲動物大遷徙的資訊
+                    <提供的內容>
+                    台灣的電池產業發展迅速，主要市場區域包括亞洲、美洲和歐洲。
+                    <回覆>
+                    unknown
+                    """,
+                    info=previous_context,
+                    model="openai:gpt-4"
+                )
+
+                while modification == "unknown":
+                    modification = input("無法判斷是否需要重新爬取資料，請問是否需要從原文重新爬取資料? (y/n)\n")
+                    if modification != "y" and modification != "n":
+                        modification = "unknown"
+                if modification == "y":
+                    new_response = generate_report(theme=report_config["theme"], titles={part:[report_config["titles"][part]]}, links=report_config["links"], rerun_process=True)
+                elif modification == "n":
+                    new_response = QA.ask_self(
+                        prompt=f"""將此內容根據以下要求進行修改，若無法達成則不要修改任何內容直接輸出原始內容，不要亂撰寫內容:
+                        ----------------
+                        {mod_command}
+                        ----------------
+
+                        範例:
+                        <原始內容>
+                        台灣的電池產業發展迅速，主要市場區域包括亞洲、美洲和歐洲。
+                        <要求>
+                        加入非洲市場區域分析。
+                        (無法達成)
+                        <修改後內容>
+                        "無法達成要求，因此不做任何修改"     <----- 這句話連同引號是必要的，以示未達成要求
+                        (空一行)                        <----- 這行是必要的，用來分隔原始內容和修改後內容
+                        台灣的電池產業發展迅速，主要市場區域包括亞洲、美洲和歐洲。(輸出與原始內容相同的內容)
+
+                        若能順利修改，則直接輸出修改後的內容，不需要加上類似"要求已達成"這句話。
+
+                        範例:
+                        <原始內容>
+                        台灣的電池產業發展迅速，主要市場區域包括亞洲、美洲和歐洲。
+                        <要求>
+                        去除跟亞洲有關資料。
+                        <修改後內容>
+                        台灣的電池產業發展迅速，主要市場區域包括美洲和歐洲。    <----- 直接輸出修改後的內容
+                        """,
+                        info=previous_context,
+                        model="openai:gpt-4"
+                    )
+                else:
+                    raise HTTPException(status_code=400, detail="無法確定是否需要重新爬取資料")
+
+                return {
+                    "original_content": previous_context,
+                    "modified_content": new_response,
+                    "part": part
+                }
+            else:
+                raise HTTPException(status_code=400, detail=f"未找到指定的部分: {part}")
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+    # print(final_result)
